df0a4977ac39d67b534c219714b0a3034f014d14
Moskvin Pavlo <79521676+Moskvin23@users.noreply.github.com>
2023-04-21 22:22:26 +0300

    I`ve added Card and styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import styles from './Card.module.scss';
-const Card = ({ title, price }) => {
+import pic2 from '../../assets/images/Plant2.png';
+import icon1 from '../../assets/icons/iconsForCard/backgroundForIcons.png';
+import basket from '../../assets/icons/iconsForCard/basket.svg';
+import like from '../../assets/icons/iconsForCard/like.svg';
+import search from '../../assets/icons/iconsForCard/search.svg';
+
+const Card = () => {
   return (
-    <div className={styles.card}>
-      <div className={styles.cardContent}>
-        <h2 className={styles.title}>{title}</h2>
-        <div className={styles.price}>${price}</div>
+    <>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardWithImage}>
+          <img alt="somePicture" src={pic2} className={styles.plantPicture} />
+          <div className={styles.backgroundForIcons}>
+            <img src={icon1} alt="icon1" className={styles.background} />
+            <img src={icon1} alt="icon1" className={styles.background} />
+            <img src={icon1} alt="icon1" className={styles.background} />
+          </div>
+          <div className={styles.icons}>
+            <img src={basket} alt="icon1" className={styles.icon} />
+            <img src={like} alt="icon1" className={styles.icon2} />
+            <img src={search} alt="icon1" className={styles.icon3} />
+          </div>
+        </div>
+        <div className={styles.cardTitleWithPrice}>
+          <h2 className={styles.title}>Barberton Daisy</h2>
+          <p className={styles.price}>$119.00</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,10 @@
+// const initialState = {
+//   // Define your initial state here
+// };
+
+// function rootReducer(state = initialState, action) {
+//   // Handle different types of actions here
+//   return state;
+// }
+
+// export default rootReducer;
