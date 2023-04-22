b705237b406d33a79ebb48d9c627d52c6523336b
Moskvin Pavlo <79521676+Moskvin23@users.noreply.github.com>
2023-04-22 10:23:59 +0300

    created regular pagination and added styles to it

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PaginationWrapper = styled.ul`
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  margin-top: 20px;
+  margin-bottom: 100px;
+  li {
+    padding: 0px 8px;
+  }
+
+  .page-link {
+    color: #333;
+    cursor: pointer;
+    text-decoration: none;
+    transition: all 0.3s ease;
+    padding: 5px 10px;
+    border-radius: 4px;
+    border: 1px solid #e5e5e5;
+  }
+
+  .page-link:hover {
+    color: #666;
+    background-color: #4ccc65;
+    color: #fff;
+    padding: 5px 10px;
+    border-radius: 4px;
+  }
+
+  .page-item.active .page-link {
+    color: #4ccc65;
+    font-weight: bold;
+  }
+`;
+const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  //href="!#"
+  return (
+    <PaginationWrapper>
+      {pageNumbers.map((number) => (
+        <li key={number} className="page-item">
+          <a onClick={() => paginate(number)} className="page-link">
+            {number}
+          </a>
+        </li>
+      ))}
+    </PaginationWrapper>
+  );
+};
+
+export default Pagination;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,8 +6,19 @@ import Sidebar from '../../components/Sidebar/Sidebar';
 import FilterPlants from '../../components/FilterPlants/FilterPlants';
 import SortingCards from '../../components/SortingCards/SortingCards';
 import Card from '../../components/Card/Card';
+import Pagination from '../../components/Pagination/Pagination';
+import { useState } from 'react';
 
 const HomePage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(4);
+  const [totalItems] = useState(16);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  // const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
   return (
     <>
       <Header />
@@ -29,6 +40,9 @@ const HomePage = () => {
             <Card />
             <Card />
             <Card />
+          </div>
+          <div className={styles.paginationSection}>
+            <Pagination itemsPerPage={itemsPerPage} totalItems={totalItems} paginate={paginate} />
           </div>
         </div>
       </div>
