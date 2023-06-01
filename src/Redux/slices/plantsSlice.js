b7ee43131fe6b80081ee4e7b27fbf2e6066efa3e
Moskvin Pavlo <79521676+Moskvin23@users.noreply.github.com>
2023-06-01 12:34:31 +0300

    Fixed bug with route

--- src/Redux/slices/plantsSlice.js
+++ src/Redux/slices/plantsSlice.js
@@ -1,50 +1,54 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios"
 
-export const fetchPlants = createAsyncThunk('plants/fetchPlants', async (params) => {
-  const { activeCategory } = params;
+export const fetchPlants = createAsyncThunk("plants/fetchPlants", async (params) => {
+  const { activeCategory } = params
   const { data } = await axios.get(
     `https://644c22f44bdbc0cc3aa333ba.mockapi.io/GreenshopAPI/Plants?name=${activeCategory?.id}`,
-  );
+  )
 
-  return data;
-});
+  return data
+})
 
 const initialState = {
   plants: [],
-  status: 'idle' | 'loading' | 'success' | 'error',
-};
+  status: "idle" | "loading" | "success" | "error",
+  selectedPlant: null,
+}
 
 const plantsSlice = createSlice({
-  name: 'plants',
+  name: "plants",
   initialState,
   reducers: {
     setPlants(state, action) {
-      state.plants = action.payload;
+      state.plants = action.payload
     },
     setSelectedPlant: (state, action) => {
-      state.selectedPlant = action.payload;
+      state.selectedPlant = action.payload
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchPlants.pending, (state) => {
-        state.status = 'loading';
-        state.plants = [];
+        state.status = "loading"
+        state.plants = []
       })
       .addCase(fetchPlants.fulfilled, (state, action) => {
-        state.status = 'success';
-        state.plants = action.payload;
+        state.status = "success"
+        state.plants = action.payload
+        if (action.payload.length > 0) {
+          state.selectedPlant = action.payload[0]
+        }
       })
       .addCase(fetchPlants.rejected, (state, action) => {
-        state.status = 'error';
-        state.plants = [];
-        console.log(action);
-      });
+        state.status = "error"
+        state.plants = []
+        console.log(action)
+      })
   },
-});
-export const selectedPlantSelector = (state) => state.plants.selectedPlant;
-export const plantsSelector = (state) => state.plants.plants;
-export const { setPlants } = plantsSlice.actions;
-export default plantsSlice.reducer;
-export const { setSelectedPlant } = plantsSlice.actions;
+})
+
+export const selectedPlantSelector = (state) => state.plants.selectedPlant
+export const plantsSelector = (state) => state.plants.plants
+export const { setPlants, setSelectedPlant } = plantsSlice.actions
+export default plantsSlice.reducer
